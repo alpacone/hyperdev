710bea5435184d7bd1f30b807a415d65f08448e9
Maxim Samburskiy <55308117+alpacone@users.noreply.github.com>
2022-02-10 14:47:04 +0500

    Save some bytes and fix `main` field

--- src/index.js
+++ src/index.js
@@ -1,25 +1,23 @@
-export const withReduxDevTools = app => {
-  return function (opts) {
-    const subs = opts.subscriptions ? opts.subscriptions : () => [];
+export const withReduxDevTools = app => opts => {
+  const subs = opts.subscriptions ? opts.subscriptions : () => [];
 
-    const devTools = window.__REDUX_DEVTOOLS_EXTENSION__.connect({ serialize: false });
+  const devTools = window.__REDUX_DEVTOOLS_EXTENSION__.connect();
 
-    const listenDevTools = dispatch => {
-      const unsubscribe = devTools.subscribe(message => {
-        if (message.type === 'DISPATCH' && message.state) {
-          requestAnimationFrame(() => dispatch(JSON.parse(message.state)));
-        }
-      });
-
-      return unsubscribe;
-    };
-
-    return app({
-      ...opts,
-      subscriptions: state => {
-        devTools.send('dispatch', state);
-        return [...subs(state), [listenDevTools]];
-      },
+  const listenDevTools = dispatch => {
+    const unsubscribe = devTools.subscribe(message => {
+      if (message.type === 'DISPATCH' && message.state) {
+        requestAnimationFrame(() => dispatch(JSON.parse(message.state)));
+      }
     });
+
+    return unsubscribe;
   };
+
+  return app({
+    ...opts,
+    subscriptions: state => {
+      devTools.send('dispatch', state);
+      return [...subs(state), [listenDevTools]];
+    },
+  });
 };
